f0b9f2330c41dd30f01cec8bd1fd096e6210476f
revertical <295559861+revertical@users.noreply.github.com>
2026-06-23 12:26:35 +0200

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include "algorithm/adjacent_find.hh"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    adjacent_find_main();
+    cout << ">>> Executed algorithm's 'adjacent_find()' function\n";
+    return 0;
+}
